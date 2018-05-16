e3ef4e2fdde8d3da474009c92009d0a477d5b3d3
Mikolaj Klonowski <nick-93@o2.pl>
2018-05-16 18:49:14 +0200

    Pierwszy commit zadania1

---
+++ src/Zadanie1.java
@@ -0,0 +1,8 @@
+class Zadanie1 {
+    public static void main(String[] args) {
+        System.out.println("uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+
+    }
+}
